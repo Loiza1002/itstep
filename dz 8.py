667f51c872676655dca4adcd7261abbf4ba4e916
Admin <lizakisaa1000@gmail.com>
2023-05-18 17:09:03 +0300

    init

---
+++ dz 8.py
@@ -0,0 +1,19 @@
+try:
+ result = []
+ def divider(a, b):
+  if a < b:
+   raise ValueError
+  if b > 100:
+   raise IndexError
+  return a/b
+ data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+ for key in data:
+  res = divider(key, data[kem])
+  result.append(res)
+
+ print(result)
+
+
+
+except (TypeError) as error:
+ print("TypeError:(")
